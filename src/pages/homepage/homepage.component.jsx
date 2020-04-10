523f892face13339effea80d175961759901d0f8
avilevant <53899685+avilevant@users.noreply.github.com>
2020-04-10 11:50:09 +0300

    first commit of react app with basci layout and first design

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './homepage.scss';
+
+const HomePage = () =>(
+    <div className="homepage">
+        <div className="cover-image"></div>
+        <div className="menu-item menu-item-1">
+            <div className="item-title">LOCATION</div>
+            <div className="item-logo item-logo-1">
+          
+            </div>
+        </div>
+        <div className="menu-item menu-item-2">
+            <div className="item-title">WEB SITE</div>
+            <div className="item-logo item-logo-2"></div>
+        </div>
+        <div className="menu-item menu-item-3">
+            <div className="item-title">FACEBOOK</div>
+            <div className="item-logo item-logo-3"></div>
+        </div>
+        <div className="menu-item menu-item-4">
+            <div className="item-title">WHATSup</div>
+            <div className="item-logo item-logo-4"></div>
+        </div>
+        <div className="menu-item menu-item-5">
+            <div className="item-title">YOUTUBE CHANNEL</div>
+            <div className="item-logo item-logo-5"></div>
+        </div>
+        <div className="menu-item menu-item-6">
+            <div className="item-title">INSTEGRAM</div>
+            <div className="item-logo item-logo-6"></div>
+        </div>
+        <div className="menu-item menu-item-7">
+            <div className="item-title">LINKDIN</div>
+            <div className="item-logo item-logo-7"></div>
+        </div>
+        <div className="menu-item menu-item-8">
+            <div className="item-title">E-MAIL</div>
+            <div className="item-logo item-logo-8"></div>
+        </div>
+    </div>
+) 
+
+export default HomePage;

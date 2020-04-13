c8d4dbf1e8ff3451457ad67e036f7dd3b5c98f80
avilevant <53899685+avilevant@users.noreply.github.com>
2020-04-13 23:18:25 +0300

    new features of routing and sign in form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-
+import { Switch,Route } from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import Testrouting from './pages/signup/signin'
+
 
 function App() {
   return (
     <div>
-    <HomePage/>
+    <Switch>
+    <Route exact path='/' component ={HomePage}/>
+    <Route path='/signup' component ={Testrouting}/>
+    </Switch>
     </div>
   );
 }

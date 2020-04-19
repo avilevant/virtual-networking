e1cc74e9188e8afa0cd43b1126009a0fd5b4f6bd
avilevant <53899685+avilevant@users.noreply.github.com>
2020-04-19 12:10:12 +0300

    testing routing

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './sign-in.scss';
+import {Redirect, Route} from 'react-router-dom';
+
 
 class SignIn extends React.Component{
     constructor(){
@@ -29,6 +31,30 @@ class SignIn extends React.Component{
             })
 
         })
+        .then(response =>response.json())
+        .then(data=>{
+            if(data==='success'){
+                
+                return(
+                                
+                <Route exact path="/signup" render={()=><Redirect to="/"/>}/>
+                
+                               
+                    
+                )
+
+                    
+                }else(
+                    prompt('user name or password incorrect')
+                )
+
+                
+                
+
+            }
+        )
+
+        console.log(this.state)
 
         
 
@@ -49,10 +75,11 @@ class SignIn extends React.Component{
             <form onSubmit={this.handleSubmit}>
             <div>
             
-            
+            <i className="fa fa-envelope icon"></i> 
             <input className='input' name='email' type='email' value={this.state.email} placeholder='email' onChange={this.handleChange} required ></input>
             </div>
             <div>
+            <i className="fa fa-key icon"></i> 
             <input className='input' name='password' type='password' value={this.state.password} placeholder='password' onChange={this.handleChange} required ></input>
             </div>
             
--- src/components/sign-up/sign-up.jsx
+++ src/components/sign-up/sign-up.jsx
@@ -5,26 +5,29 @@ class SignUp extends React.Component{
     constructor(){
     super()
 
-    this.state = {
-        name:'',
-        phone:'',
-        email:'',
-        password:'',
-        validPassword:''
-
-    } 
+    
+        
+        this.state = {
+            name:'',
+            phone:'',
+            email:'',
+            password:'',
+            validPassword:''
+    
+        } 
+  
 } 
      
     handleSubmit = (event) => {
         event.preventDefault();
 
-   /*     if(this.state({password === validPassword})){
+     if(this.state.password === this.state.validPassword){
         
-
-            this.setState({name:'', email:'',password:'',phone:'',validPassword:''})     
+        console.log(this.state)
+               
         }else{
             prompt('passwords dont match')
-        }*/
+        }
         this.setState({name:'', email:'',password:'',phone:'',validPassword:''})
 
 
@@ -43,23 +46,27 @@ class SignUp extends React.Component{
             <form onSubmit={this.handleSubmit}>
             
             <div>
-           
+            <i className="fa fa-user icon"></i> 
             <input className='input' name='name' type='string' value={this.state.name} placeholder='Full Name' onChange={this.handleChange} required ></input>
             </div>
 
             <div>
+            <i className="fa fa-phone icon"></i> 
             <input className='input' name='phone' type='string' value={this.state.phone} placeholder='Phone Number' onChange={this.handleChange} required ></input>
             </div>
 
             <div> 
+            <i className="fa fa-envelope icon"></i> 
             <input className='input' name='email' type='email' value={this.state.email} placeholder='email' onChange={this.handleChange} required ></input>
             </div>
 
             <div>
+            <i className="fa fa-key icon"></i> 
             <input className='input' name='password' type='password' value={this.state.password} placeholder='password' onChange={this.handleChange} required ></input>
             </div>
 
             <div>
+            <i className="fa fa-key icon"></i> 
             <input className='input' name='validPassword' type='password' value={this.state.validPassword} placeholder='validPassword' onChange={this.handleChange} required ></input>
             </div>
             
